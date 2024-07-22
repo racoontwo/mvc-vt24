66ac748afed628727142d375d6de9cabf65fb9dd
racoon2 <oliver.roosvall@gmail.com>
2024-07-22 14:35:51 +0200

    fixed the structure for the different css-style for the project.

--- importmap.php
+++ importmap.php
@@ -16,6 +16,10 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'project' => [
+        'path' => './assets/project.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
